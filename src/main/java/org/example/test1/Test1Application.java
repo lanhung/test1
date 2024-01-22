7f3dbbc706ff95bfa02530da75ddd6163ab84a80
123uiui <135583175+123uiui@users.noreply.github.com>
2024-01-22 14:46:33 +0800

    修改了输出的日志7

--- src/main/java/org/example/test1/Test1Application.java
+++ src/main/java/org/example/test1/Test1Application.java
@@ -8,7 +8,7 @@ public class Test1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Test1Application.class, args);
-        System.out.printf("hello world6");
+        System.out.printf("hello world7");
 
     }
 
